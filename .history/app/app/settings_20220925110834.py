0ab5975a55fd1cbe2468d9a0b480f6144cb72d8e
masamichan <positivemasa031@gmail.com>
2022-09-25 11:16:41 +0900

    wirtting setting.py

--- .history/app/app/settings_20220925110834.py
+++ .history/app/app/settings_20220925110834.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAMAE': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
