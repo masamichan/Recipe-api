d95b8feb13b4b7b9234e22820c3f2b33b5133147
masamichan <positivemasa031@gmail.com>
2022-09-25 13:41:49 +0900

    wirte test for wait_for_db_command(2)

--- .history/app/core/test/test_command_20220925133722.py
+++ .history/app/core/test/test_command_20220925133722.py
@@ -4,14 +4,14 @@ Test custom Django management commands.
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('core.management.commands.wait_for_db.Command.Check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
@@ -21,15 +21,15 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(database=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_ready(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_call_with(database=['default'])
